82d699176af5f3ce0c41b43f16687e07c36ef887
rab28ina <48763921+rab28ina@users.noreply.github.com>
2019-04-06 22:53:44 +0530

    Add files via upload

---
+++ lib/quiz1.dart
@@ -0,0 +1,286 @@
+import 'package:flutter/material.dart';
+
+
+var finalScore = 0;
+var questionNumber = 0;
+var quiz = new AnimalQuiz();
+
+class AnimalQuiz{
+  var images = [
+    "roger_federer"];
+
+
+
+  var questions = [
+    "Which of the following player is currently ranked world No. 4 in men's singles tennis by the Association of Tennis Professionals?",
+    "Who has become the first Indian cricketer to cross 8,000 runs in T20s?",
+    "Who has become the first-ever player to smash 500 sixes in international cricket?",
+    "Which Indian sportsperson has been selected as Barbie role model?",
+  ];
+
+  //      'You have pushed the button $_counter time${_counter != 1 ? 's' : ''}:',
+
+  var choices = [
+    ["Roger Federer", "Rafael Nadal", "Novac Djokovic", "Andy Murray"],
+    ["Rohit Sharma", "Virat Kohli", "Suresh Raina", "M.S Dhoni"],
+    ["Virat Kohli", "Chris Gayle", "Jos Buttler", "M.S Dhoni"],
+    ["Saina Nehwal", "Dipa Karmakar", "Mithali Raj", "Mary Kom"]
+  ];
+
+
+  var correctAnswers = [
+    "Roger Federer", "Suresh Raina", "Chris Gayle", "Dipa Karmakar"
+  ];
+}
+
+class Quiz1 extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return new Quiz1State();
+  }
+}
+
+class Quiz1State extends State<Quiz1> {
+  @override
+  Widget build(BuildContext context) {
+    return new WillPopScope(
+        onWillPop: () async => false,
+        child: Scaffold(
+
+          body: new Container(
+            margin: const EdgeInsets.all(10.0),
+            alignment: Alignment.topCenter,
+            child: new Column(
+              children: <Widget>[
+                new Padding(padding: EdgeInsets.all(20.0)),
+
+                new Container(
+                  alignment: Alignment.centerRight,
+                  child: new Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+
+                      new Text("Question ${questionNumber + 1} of ${quiz.questions.length}",
+                        style: new TextStyle(
+                            fontSize: 22.0
+                        ),),
+
+                      new Text("Score: $finalScore",
+                        style: new TextStyle(
+                            fontSize: 22.0
+                        ),)
+                    ],
+                  ),
+                ),
+
+
+                //image
+                new Padding(padding: EdgeInsets.all(10.0)),
+
+
+                new Padding(padding: EdgeInsets.all(10.0)),
+
+                new Text(quiz.questions[questionNumber],
+                  style: new TextStyle(
+                    fontSize: 20.0,
+                  ),),
+
+                new Padding(padding: EdgeInsets.all(10.0)),
+
+                new Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+
+                    //button 1
+                    new MaterialButton(
+                      minWidth: 120.0,
+                      color: Colors.blueGrey,
+                      onPressed: (){
+                        if(quiz.choices[questionNumber][0] == quiz.correctAnswers[questionNumber]){
+                          debugPrint("Correct");
+                          finalScore++;
+                        }else{
+                          debugPrint("Wrong");
+                        }
+                        updateQuestion();
+                      },
+                      child: new Text(quiz.choices[questionNumber][0],
+                        style: new TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.white
+                        ),),
+                    ),
+
+                    //button 2
+                    new MaterialButton(
+                      minWidth: 120.0,
+                      color: Colors.blueGrey,
+                      onPressed: (){
+
+                        if(quiz.choices[questionNumber][1] == quiz.correctAnswers[questionNumber]){
+                          debugPrint("Correct");
+                          finalScore++;
+                        }else{
+                          debugPrint("Wrong");
+                        }
+                        updateQuestion();
+                      },
+                      child: new Text(quiz.choices[questionNumber][1],
+                        style: new TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.white
+                        ),),
+                    ),
+
+                  ],
+
+                ),
+
+                new Padding(padding: EdgeInsets.all(10.0)),
+
+                new Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+
+                    //button 3
+                    new MaterialButton(
+                      minWidth: 120.0,
+                      color: Colors.blueGrey,
+                      onPressed: (){
+
+                        if(quiz.choices[questionNumber][2] == quiz.correctAnswers[questionNumber]){
+                          debugPrint("Correct");
+                          finalScore++;
+                        }else{
+                          debugPrint("Wrong");
+                        }
+                        updateQuestion();
+                      },
+                      child: new Text(quiz.choices[questionNumber][2],
+                        style: new TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.white
+                        ),),
+                    ),
+
+                    //button 4
+                    new MaterialButton(
+                      minWidth: 120.0,
+                      color: Colors.blueGrey,
+                      onPressed: (){
+
+                        if(quiz.choices[questionNumber][3] == quiz.correctAnswers[questionNumber]){
+                          debugPrint("Correct");
+                          finalScore++;
+                        }else{
+                          debugPrint("Wrong");
+                        }
+                        updateQuestion();
+                      },
+                      child: new Text(quiz.choices[questionNumber][3],
+                        style: new TextStyle(
+                            fontSize: 20.0,
+                            color: Colors.white
+                        ),),
+                    ),
+
+                  ],
+                ),
+
+                new Padding(padding: EdgeInsets.all(15.0)),
+
+                new Container(
+                    alignment: Alignment.bottomCenter,
+                    child:  new MaterialButton(
+                        minWidth: 240.0,
+                        height: 30.0,
+                        color: Colors.red,
+                        onPressed: resetQuiz,
+                        child: new Text("Quit",
+                          style: new TextStyle(
+                              fontSize: 18.0,
+                              color: Colors.white
+                          ),)
+                    )
+                ),
+
+
+
+
+              ],
+            ),
+          ),
+
+        )
+    );
+  }
+
+  void resetQuiz(){
+    setState(() {
+      Navigator.pop(context);
+      finalScore = 0;
+      questionNumber = 0;
+    });
+  }
+
+
+
+  void updateQuestion(){
+    setState(() {
+      if(questionNumber == quiz.questions.length - 1){
+        Navigator.push(context, new MaterialPageRoute(builder: (context)=> new Summary(score: finalScore,)));
+
+      }else{
+        questionNumber++;
+      }
+    });
+  }
+}
+
+
+class Summary extends StatelessWidget{
+  final int score;
+  Summary({Key key, @required this.score}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return new WillPopScope(
+      onWillPop: () async => false,
+      child: Scaffold(
+
+        body: new Container(
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+
+              new Text("Final Score: $score",
+                style: new TextStyle(
+                    fontSize: 35.0
+                ),),
+
+              new Padding(padding: EdgeInsets.all(30.0)),
+
+              new MaterialButton(
+                color: Colors.red,
+                onPressed: (){
+                  questionNumber = 0;
+                  finalScore = 0;
+                  Navigator.pop(context);
+                },
+                child: new Text("Reset Quiz",
+                  style: new TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white
+                  ),),)
+
+            ],
+          ),
+        ),
+
+
+      ),
+    );
+  }
+
+
+}
